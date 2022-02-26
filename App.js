c6a883d8713dd8c0f64d52ef46c3ad13c8152b02
zerex1502 <zerex1502@gmail.com>
2022-02-26 09:29:44 -0500

    first commit

--- App.js
+++ App.js
@@ -14,7 +14,53 @@ constructor() // permite iniciar variables a nivel global de la clase
   {
     nombre: "",
     apellido: ""
-  }
+  };
 }  
 
+//ahora vamos a crear la vista para ver los componentes
+//que deseamos ver
+render()
+{
+    return(
+      <View style= {estilos.Container}>
+        <Text>estamos probando los estados de las variables</Text>
+        <TextInput value= {this.state.nombre}
+        onChangeText={(textonuevo)=>{
+        this.setState(
+          {
+          nombre: textonuevo
+        }
+        )
+      }
+    }
+        ></TextInput>
+
+<TextInput value= {this.state.apellido}
+        onChangeText={(textonuevo)=>{
+        this.setState(
+          {
+          apellido: textnuevo
+        }
+        )
+      }
+    }
+        ></TextInput>
+
+      </View>
+    )
 }
+
+}
+
+const estilos = StyleSheet.create(
+  {
+    Container:
+    {
+      flex:1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center'
+  
+    }
+  }
+)
